69e460b28ad3be7157e2ba92cbcd4a6b14937132
phasenull <55253035+phasenull@users.noreply.github.com>
2023-05-14 12:46:32 +0300

    path typo

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-const WSOH = require("js/classes/App.js");
-const Connection = require("js/classes/Connection.js");
+const WSOH = require("./js/classes/App");
+const Connection = require("./js/classes/Connection");
 export {WSOH, Connection}
